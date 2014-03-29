2f10d9dddd263e9e48cbf938bdacef54e9841fd7
spmno <sunqingpeng@hotmail.com>
2014-03-29 14:12:29 +0800

    change way to use android AsyncTask

--- src/com/dawnstep/followme/MainActivity.java
+++ src/com/dawnstep/followme/MainActivity.java
@@ -2,6 +2,7 @@ package com.dawnstep.followme;
 
 import java.io.IOException;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -23,6 +24,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	private TextView resultText;
 	private Double longitude = 123.4;
 	private Double latitude = 23.44; 
+	private HttpTask httpTask;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -41,25 +43,41 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	@Override
 	public void onClick(View view) {
-		 String httpUrl = "http://127.0.0.1:8000/location/?name="+"spmno"+"&longitude="+longitude.toString()+"&latitude="+latitude.toString();   
-         //创建httpRequest对象
-         HttpGet httpRequest = new HttpGet(httpUrl);
-         
-         try {
-        	 HttpClient httpClient = new DefaultHttpClient();
-        	 HttpResponse httpResponse = httpClient.execute(httpRequest);
-        	 if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-        		 String strResult = EntityUtils.toString(httpResponse.getEntity());
-        		 resultText.setText(strResult);
-        	 } else {
-        		 resultText.setText("request error");
-        	 }
-         } catch (ClientProtocolException e) {
-        	 resultText.setText(e.getMessage().toString());
-         } catch (IOException e) {
-        	 resultText.setText(e.getMessage().toString());
-         }
+		httpTask = new HttpTask();
+		httpTask.execute("hello");
+	}
+	
+	private class HttpTask extends AsyncTask<String, Integer, String> {
+		
+		@Override
+		protected void onPostExecute(String result) {
+			resultText.setText(result);
+		}
 
+		@Override
+		protected String doInBackground(String... arg0) {
+			// TODO Auto-generated method stub
+			 String httpUrl = "http://192.168.2.151:8000/location/?name="+"spmno"+"&longitude="+longitude.toString()+"&latitude="+latitude.toString();   
+	         //创建httpRequest对象
+	         HttpGet httpRequest = new HttpGet(httpUrl);
+	         String result;
+	         try {
+	        	 HttpClient httpClient = new DefaultHttpClient();
+	        	 HttpResponse httpResponse = httpClient.execute(httpRequest);
+	        	 if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+	        		 result = EntityUtils.toString(httpResponse.getEntity());
+	        		 
+	        	 } else {
+	        		 result = "request error";
+	        	 }
+	         } catch (ClientProtocolException e) {
+	        	 result = e.getMessage().toString();
+	         } catch (IOException e) {
+	        	 result = e.getMessage().toString();
+	         }
+			return result;
+		}
+		
 	}
 
 }
