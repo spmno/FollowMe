8d8f9549ea3dcb5aabeea88bda62a8238285185b
spmno <sunqingpeng@hotmail.com>
2014-04-01 08:55:47 +0800

    增加位置。

--- src/com/dawnstep/followme/MainActivity.java
+++ src/com/dawnstep/followme/MainActivity.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,6 +20,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import com.datacool.LocationManager.LBSTool;
+import com.datacool.LocationManager.LocationData;
+
 public class MainActivity extends Activity implements OnClickListener {
 
 	private Button sendLocationButton;
@@ -25,6 +30,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	private Double longitude = 123.4;
 	private Double latitude = 23.44; 
 	private HttpTask httpTask;
+	private String phoneNumber;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,6 +38,8 @@ public class MainActivity extends Activity implements OnClickListener {
 		sendLocationButton = (Button)findViewById(R.id.sendButton);
 		sendLocationButton.setOnClickListener(this);
 		resultText = (TextView)findViewById(R.id.textView1);
+		TelephonyManager tm = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+		phoneNumber  = tm.getLine1Number();//获取本机号码
 	}
 
 	@Override
@@ -57,24 +65,27 @@ public class MainActivity extends Activity implements OnClickListener {
 		@Override
 		protected String doInBackground(String... arg0) {
 			// TODO Auto-generated method stub
-			 String httpUrl = "http://192.168.2.151:8000/location/?name="+"18640244302"+"&longitude="+longitude.toString()+"&latitude="+latitude.toString();   
-	         //创建httpRequest对象
-	         HttpGet httpRequest = new HttpGet(httpUrl);
-	         String result;
-	         try {
-	        	 HttpClient httpClient = new DefaultHttpClient();
-	        	 HttpResponse httpResponse = httpClient.execute(httpRequest);
-	        	 if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-	        		 result = EntityUtils.toString(httpResponse.getEntity());
+			
+			LBSTool lbs = new LBSTool(MainActivity.this);
+			LocationData location = lbs.getLocation(30000);
+			String httpUrl = "http://192.168.8.200:8000/location/?name="+phoneNumber+"&longitude="+location.longitude+"&latitude="+location.latitude;   
+			//创建httpRequest对象
+	        HttpGet httpRequest = new HttpGet(httpUrl);
+	        String result;
+	        try {
+	        	HttpClient httpClient = new DefaultHttpClient();
+	        	HttpResponse httpResponse = httpClient.execute(httpRequest);
+	        	if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+	        		result = EntityUtils.toString(httpResponse.getEntity());
 	        		 
-	        	 } else {
-	        		 result = "request error";
-	        	 }
-	         } catch (ClientProtocolException e) {
-	        	 result = e.getMessage().toString();
-	         } catch (IOException e) {
-	        	 result = e.getMessage().toString();
-	         }
+	        	} else {
+	        		result = "request error";
+	        	}
+	        } catch (ClientProtocolException e) {
+	        	result = e.getMessage().toString();
+	        } catch (IOException e) {
+	        	result = e.getMessage().toString();
+	        }
 			return result;
 		}
 		
